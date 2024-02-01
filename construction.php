80800cfd0cfbd79e81685ba824a243fc2e8dd45c
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-02-01 22:52:47 +0100

    Create construction.php

---
+++ construction.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/887e44e44e.js" crossorigin="anonymous"></script>
+
+<div class="wrapper">
+<i class="fa-solid fa-person-digging fa-4x"></i><br>
+<h1>This section of the website is under construction...</h1>
+</div>
+
+<style>
+    .wrapper{
+    color:white;
+    display: flex;
+    justify-content: center;    
+    align-items: center;
+    margin-top: 7%;
+    margin-left: 25%;
+    width: 50%;
+    height: 30%;
+    position:relative;
+    background-color: var(--decor_main);
+    border-radius: 1%;
+}
+</style>
+
+<?php
+    header( "refresh:1;url=index.php" );
+
+?>
