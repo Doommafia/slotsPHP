6bd23794c82184338efc6ad3122b7b1fffe897b6
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-02-01 22:51:32 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,31 +1,20 @@
-<?php
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $random = '';
-    for( $i = 0; $i < 8; $i++ ){
-        $random .= $characters[rand(0, strlen($characters) -1)];
-    }
-
-?>
-
-
 <link rel="stylesheet" href="style.css">
 <script src="https://kit.fontawesome.com/887e44e44e.js" crossorigin="anonymous"></script>
 
-<div class="wrapper" action="main.php">
+<div class="wrapper">
     <div class="inner">
         <i class="fa-solid fa-person-digging fa-4x"></i>
         <h1>Welcome to the mines!</h1>
-        <form action="main.php">
-            Username</br>
-            <input type="text"></input></br>
-            Password</br>
-            <input type="text"></input></br>
-            Email</br>
-            <input type="text"></input></br>
-            Please type out <?php echo $random; ?>.</br>
-            <input type="text"></input></br>
-            <input type="submit" value="Join" class="submitBtn"></input>
+        <form action="signup2.php" method="POST">
+            Username<br>
+            <input type="text" name="username"></input><br>
+            Password<br>
+            <input type="password" name="password"></input><br>
+            Email<br>
+            <input type="email" name="email"></input><br>
+            <input type="submit" name="submit" class="submitBtn"></input>
         </form>
+
 
     </div>
 </div>
