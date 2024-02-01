d5d5ee91f1f4de310a6d0b752efdcdf2f7e6eec3
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-02-01 22:51:41 +0100

    Create signup2.php

---
+++ signup2.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/887e44e44e.js" crossorigin="anonymous"></script>
+<?php
+    include_once 'dbConn.php';
+    if (isset($_POST['submit']) && $_POST['username'] != null && $_POST['password']!= null && $_POST['email']!= null) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        $stmt = $conn->prepare("INSERT INTO users (username, password, email, money) VALUES (?, ?, ?, 0)");
+        $stmt->bind_param("sss", $username, $password, $email);
+        
+        if ($stmt->execute()) {
+            echo "Account created successfully!";
+            header( "refresh:1;url=index.php" );
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    }
+
+
+
+?>
+
+
