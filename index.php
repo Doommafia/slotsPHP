f476b535469c78bf95307bdbd920e540878faf45
Fisnik <71096106+Doommafia@users.noreply.github.com>
2024-02-01 22:52:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
+<?php
+session_start();
+echo $_SESSION['username'];
+echo $_SESSION['password'];
+echo $_SESSION['email'];
+echo $_SESSION['money'];
+session_abort();
 
+?>
+
+
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/887e44e44e.js" crossorigin="anonymous"></script>
+
+<h1>Welcome to the slots website!</h1>
+<a href="slots.php" class="solo">Solo</a>
+<a href="construction.php" class="versus">Versus</a>
+
+
+<style>
+    :root {
+    text-align:center;
+    color:white;
+    text-decoration: none;
+    
+    }
+    h1 {
+        text-align: center;
+        color: white;
+        margin-top: 10%
+    }
+
+</style>
